703ed3783408026180b9d1a438453cd4852c0afa
Rayen Troudi <94689247+kugeIblitz@users.noreply.github.com>
2023-03-28 05:59:37 +0100

    A

---
+++ produits/src/main/java/com/produits/repos/ProduitRepository.java
@@ -0,0 +1,9 @@
+package com.produits.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.produits.entities.Produit;
+
+public interface ProduitRepository extends JpaRepository<Produit, Long> {
+
+}
